f85865c6a67fec38f68902b07a46aa986e99e392
Jake Zerrer <him@jakezerrer.com>
2018-02-19 07:07:20 -0800

    Copy and content updates

--- src/cljs/portfolio/views.cljs
+++ src/cljs/portfolio/views.cljs
@@ -2,14 +2,24 @@
   (:require [re-frame.core :as re-frame]
             [goog.string :as gstring]
             [portfolio.cdn :refer [resource-url]]
+            ;; [portfolio.router :refer [r]]
             [goog.string.format]))
 
-(declare portfolio)
+(declare portfolio home)
 
 (defn main-panel []
-  [:div.container [:div.portfolio (portfolio)]])
+  [:div.container (home) (portfolio)])
 
-(defn home [])
+(defn home []
+  [:div.home__wrap
+   [:div.home
+    [:h1.home__header "Jake Zerrer"]
+    [:nav.home__links
+     [:ul
+      [:li [:a {:href "/portfolio"} "Portfolio"]]
+      [:li [:a {:href "https://www.linkedin.com/in/jakezerrer/"} "linkedin"]]
+      [:li [:a {:href "https://www.instagram.com/telekid/"} "instagram"]]
+      [:li [:a {:href "https://github.com/telekid"} "github"]]]]]])
 
 (defn company [& {:keys [name slug description content show-nav] :or {show-nav false}}]
   [:div.project
@@ -57,7 +67,7 @@
   [:ul.project__skills (map-indexed (fn [idx skill] [:li.project__skill {:key idx} skill]) skills)])
 
 (defn portfolio []
-  [:div
+  [:div.portfolio
         (company :name "untapt"
                  :show-nav true
                  :slug "untapt"
@@ -66,17 +76,28 @@
                            (description "At untapt, I oversaw design and development for two large web
                                          applications used by over 30,000 software engineers and dozens
                                          of technology companies.")
-                           (description "The first web app helped software engineers
+                           (description "The "
+                                        [:i "Candidate Interface"]
+                                        " app helped software engineers
                                          build beautiful resumes and apply to technology roles.
                                          The application leveraged ML to help candidates increase
                                          their odds of landing a successful interview.")
                            (description "The second application was a lightweight "
                                    [:abbr {:title "Applicant Tracking System"} "ATS"]
                                    " that hiring managers used to review and manage
-                                   applications.")
+                                   job applications. This "
+                                   [:i "Manager Interface"]
+                                   " was designed with ease-of-use and rapid review in mind.")
                            (skills ["Figma" "Photoshop" "Marvel" "AngularJS"])
                            (subsection "Resume Editor")
-                           (description "A key feature of the untapt candidate experience is the Resume Editor. It
+                           (description "When designing the editor, we focused
+                                         on making editing quick and intuitive. Editing a
+                                         section is as easy as clicking on the content that
+                                         you would like to change:")
+                           (example-video :filename "aBUoGM_Z0kY"
+                                          :caption "Example of automatic skill inference")
+                           (description "A key feature of the " [:i "Candidate Experience"]
+                                        " was the Resume Editor. It
                                          allows engineers to quickly build a resume that
                                          they can later send to companies.")
                            (description "The editor provides feedback to candidates to help
@@ -91,24 +112,21 @@
                                          manually. Skills required by a specific role are
                                          highlighted in orange:")
                            (example-video :filename "1Mi-tY-Vchc"
-                                          :caption "Resume Recommendations")
-                           (description "When designing the editor, we focused
-                                         on making editing quick and intuitive. Editing a
-                                         section is as easy as clicking on the content that
-                                         you would like to change:")
-                           (example-video :filename "aBUoGM_Z0kY"
-                                          :caption "Example of automatic skill inference")
-                           (subsection "Hiring Manager Interface")
-                           (description "")
-                           (example-image :filename "manager-landing.png"
-                                          :caption "Mockup of our marketing website for hiring managers"
-                                          :width 600)
-                           (example-image :filename "manager-pipeline.png"
-                                          :caption "Interface for quickly navigating through job applications"
-                                          :width 600)
-                           (example-image :filename "resume-versions.png"
-                                          :caption "Resume editor, showing users' ability to manage multiple resumes"
-                                          :width 600)])
+                                          :caption "Resume Recommendations")])
+                           ;; (subsection "Hiring Manager Interface")
+                           ;; (description "The "
+                           ;;              [:i "Hiring Manager Interface"])
+                           ;;              " "
+
+                           ;; (example-image :filename "manager-landing.png"
+                           ;;                :caption "Mockup of our marketing website for hiring managers"
+                           ;;                :width 600)
+                           ;; (example-image :filename "manager-pipeline.png"
+                           ;;                :caption "Interface for quickly navigating through job applications"
+                           ;;                :width 600)
+                           ;; (example-image :filename "resume-versions.png"
+                           ;;                :caption "Resume editor, showing users' ability to manage multiple resumes"
+                           ;;                :width 600)])
         (company :name "vodka"
                  :slug "vodka"
                  :description "Custom iPad application for Broadway show \"Natasha, Pierre and the Great Comet of 1812\"."
@@ -116,8 +134,16 @@
                            (description "Nick Pope, sound designer for the Broadway show "
                                         [:i "Natasha, Pierre and the Great Comet of 1812,"]
                                         " approached me to help him solve an unusual design
-                                          challenge.")
-
+                                        problem.")
+                           (description "Unlike most Broadway shows, the actors in "
+                                        [:i "Great Comet"]
+                                        " moved not only on stage, but around the audience. Nick needed
+                                         to find a way to position the voices of actors and musicians within
+                                         the 2D plane that makes up the ground plan of the theater.")
+                           (description "Though he had a sophisticated computerized mixing system that could
+                                         take care of the actual audio computations (like panning and summing),
+                                         the native software interface was too cumbersome to allow them make
+                                         changes quickly.")
                            (skills ["React Native" "Figma" "Swift"])
                            (example-image :filename "vodka-screenshot.jpg"
                                           :caption "Control surface for realtime two-dimensional audio placement"
@@ -128,20 +154,31 @@
                  :content [
                            (section "Refuge")
                            (description "Refuge was a side project that nearly snowballed into a startup.")
+                           (description "The goal of Refuge was to build a social music platform that focused
+                                         on helping users find, share and support new artists. The platform would
+                                         help artists build a brand and gain exposure without the help of a
+                                         record label.")
                            (description "Though Refuge never left the design & prototyping phase, to date, it remains one of my most beloved projects.")
-
-                           (skills ["Figma"])
-                           (example-image :filename "refuge-landing.png"
-                                          :caption "the refu.ge home page"
-                                          :width 600)
-                           (example-image :filename "refuge-live.png"
-                                          :caption "See what musical tastemakers are listening to"
-                                          :width 300)
-                           (example-image :filename "refuge-playback.png"
-                                          :caption "Simple playback interface"
-                                          :width 300)
-                           (example-image :filename "refuge-color-study.png"
-                                          :caption "Color study for Refuge"
-                                          :width 450)])])
-
+                           (description "To learn more about Refuge, check out our "
+                                         [:a {:href "https://paper.dropbox.com/doc/Refuge-Overview-for-YC-R61kLiZFtC1MiOzmxPNjM"}]
+                                         "Y Combinator Pitch Document"
+                                         "(note: may have trouble loading) and a sample "
+                                         [:a {:href "https://projects.invisionapp.com/share/RBF07PMDY#/screens/269781422_Live_Feed"}]
+                                         "Invision Prototype"
+                                         ".")
+                           (skills ["Figma", "Invision"])])])
+                           ;; (subsection "Selected Prototype Screenshots")
+                           ;; (description "Early Refuge prototyping took place during a rapid ")])])
+                           ;; (example-image :filename "refuge-landing.png"
+                           ;;                :caption "the refu.ge home page"
+                           ;;                :width 600)
+                           ;; (example-image :filename "refuge-live.png"
+                           ;;                :caption "See what musical tastemakers are listening to"
+                           ;;                :width 300)
+                           ;; (example-image :filename "refuge-playback.png"
+                           ;;                :caption "Simple playback interface"
+                           ;;                :width 300)
+                           ;; (example-image :filename "refuge-color-study.png"
+                           ;;                :caption "Color study for Refuge"
+                           ;;                :width 450)])])
 
